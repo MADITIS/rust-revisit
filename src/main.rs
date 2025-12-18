eb3e4b8f57cb59a9ab4da148225cd5709ca2d87e
manik <manik6418@gmail.com>
2025-12-18 09:48:06 -0800

    guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+
+    let scret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("please input your guess: {scret_number}");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+
+    println!("your guess: {guess}")
 }
